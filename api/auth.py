f6f2a1dee716f0b10ae8307f20381125bba31f92
Michal Proszek <michal.proszek@gmail.com>
2015-11-27 22:14:46 +0100

    auth: Add a custom login_required decorator

--- api/auth.py
+++ api/auth.py
@@ -1,17 +1,20 @@
 import json
 from datetime import datetime, timedelta
 
+from functools import wraps
 import requests
 import jwt
 from django.conf import settings
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
-
 from rest_framework.parsers import JSONParser
+
+from jwt import DecodeError, ExpiredSignature
 
 from api.models import User
 from api.serializers import GoogleAuthSerializer
-from api.errors import AuthenticationError
+from api.errors import AuthenticationError, AuthorizationError, \
+    InvalidRequestData
 
 
 def create_token(user):
@@ -24,12 +27,38 @@ def create_token(user):
     return token.decode('unicode_escape')
 
 
+def parse_token(request):
+    try:
+        token = request.META.get('HTTP_AUTHORIZATION').split()[1]
+    except IndexError:
+        raise InvalidRequestData('Incomplete Authorization header.')
+
+    return jwt.decode(token, settings.SECRET_KEY)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(request, *args, **kwargs):
+        if not request.META.get('HTTP_AUTHORIZATION'):
+            raise InvalidRequestData('Missing Authorization header.')
+
+        try:
+            payload = parse_token(request)
+        except DecodeError:
+            raise AuthorizationError('Token is invalid.')
+        except ExpiredSignature:
+            raise AuthorizationError('Token has expired.')
+
+        user = User.objects.get(id=payload['sub'])
+        return f(request, user, *args, **kwargs)
+
+    return decorated_function
+
+
 @api_view(('POST',))
 @parser_classes((JSONParser,))
 def google(request):
-    """
-    Google OAuth2
-    """
+    """API authentication using Google OAuth2"""
     serializer = GoogleAuthSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
 
