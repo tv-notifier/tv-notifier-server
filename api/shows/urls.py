9db717dc6385f386249df9412cea2a4813b72b85
Michal Proszek <michal.proszek@gmail.com>
2016-01-11 21:29:37 +0100

    shows(urls): Correct /shows/:id's regex

--- api/shows/urls.py
+++ api/shows/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<show_id>\d+)/follow$', views.follow, name='follow'),
-    url(r'^(?P<show_id>\d+)', views.info, name='info')
+    url(r'^(?P<show_id>\d+)$', views.info, name='info'),
+    url(r'^(?P<show_id>\d+)/follow$', views.follow, name='follow')
 ]
